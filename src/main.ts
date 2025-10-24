28db3eef2f061053436448754e785761c0c8dea3
nikita-dogil <ndogil243@gmail.com>
2025-10-24 15:41:12 +0300

    add ssl

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { readFileSync } from 'node:fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const sslKey = process.env.SSL_KEY;
+  const sslCert = process.env.SSL_CERT;
+
+  const isSsl = sslKey && sslCert;
+
+  const options = isSsl
+    ? {
+        httpsOptions: {
+          key: readFileSync(sslKey),
+          cert: readFileSync(sslCert),
+        },
+      }
+    : {};
+
+  const app = await NestFactory.create(AppModule, options);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
