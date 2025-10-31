d3aaa4b660808058b385ba5093320fca09b520ee
nikita-dogil <ndogil243@gmail.com>
2025-10-31 12:14:18 +0300

    fix sync:labels command

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GithubModule } from './github/github.module';
 import { ConfigModule } from '@nestjs/config';
+import { CommandModule } from 'nestjs-command';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), GithubModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GithubModule,
+    CommandModule,
+  ],
   controllers: [],
   providers: [],
 })
